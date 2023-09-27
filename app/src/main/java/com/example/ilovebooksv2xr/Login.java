ba9990fab4283c36e7139cc0cb5cb9b9373e3f7b
Kaoru X <KaoruXR.gmail.com>
2023-09-27 02:09:09 -0300

    ActualizacionV3ElProgramaEsFuncional

--- app/src/main/java/com/example/ilovebooksv2xr/Login.java
+++ app/src/main/java/com/example/ilovebooksv2xr/Login.java
@@ -5,16 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.CheckBox;
+import android.widget.Toast;
 public class Login extends AppCompatActivity {
-
+    private Button b;
+    private RatingBar ex;
+    private CheckBox c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        b = (Button) findViewById(R.id.b2);
+        ex = (RatingBar) findViewById(R.id.rex);
+        c = (CheckBox) findViewById(R.id.c1);
+
+        ch();
     }
     public void menu(View v){
         Intent menu = new Intent(this,MainActivity.class);
         startActivity(menu);
     }
+
+    public void ch(){
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this,"¡De Recordare! 7w7",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void extrellas(View v){
+        Toast.makeText(Login.this,"Pusiste: "+ex.getRating()+"Arigato :3",Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/com/example/ilovebooksv2xr/Register.java
+++ app/src/main/java/com/example/ilovebooksv2xr/Register.java
@@ -5,18 +5,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.Button;
+import java.util.TimerTask;
+import java.util.Timer;
 
 public class Register extends AppCompatActivity {
-
+    private RadioButton ra1,ra2;
+    private ProgressBar p;
+    int aux = 0;
+    private Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ra1 = (RadioButton) findViewById(R.id.r1);
+        ra2 = (RadioButton) findViewById(R.id.r2);
+        p = (ProgressBar) findViewById(R.id.pbar);
+        b = (Button) findViewById(R.id.b1);
+
+        radios1();
+        radios2();
+        barra();
     }
 
     public void menu(View v){
         Intent menu = new Intent(this,MainActivity.class);
         startActivity(menu);
+    }
+
+    public void radios1(){
+        ra1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Register.this,"Eres una mujer :D",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void radios2(){
+        ra2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Register.this,"Eres un Hombre :D",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void barra(){
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Timer t = new Timer();
+                TimerTask tt = new TimerTask(){
+                    @Override
+                    public void run(){
+                        aux++;
+                        p.setProgress(aux);
+                        if(aux==100)
+                            t.cancel();
+                    }
+                };
+                t.schedule(tt,0,100);
+            }
+        });
     }
 
 }
